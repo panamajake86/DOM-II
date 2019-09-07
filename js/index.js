056723b59e3a70f0d3e66ebe2d36ee68294e5c79
Jake <panamajake86@gmail.com>
2019-09-06 20:23:45 -0600

    Gotten 4 event listeners included in the index.js file. Have 6 more to go.  Shouldn't be too hard.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+const btnBtn = document.querySelectorAll(".btn");
+for(var i = 0; i < btnBtn.length; i++){
+    btnBtn[i].addEventListener('click', (event)=> {
+    console.log("That tickles!");
+    })
+};
+
+const grow = document.querySelectorAll('h4');
+grow[0].addEventListener('mouseover', (event) => {
+    grow[0].style.transform = 'scale(1.5)';
+});
+grow[0].addEventListener('mouseout', (event) => {
+    grow[0].style.transform = 'scale(1)';
+});
+
+grow[1].addEventListener('mouseover', (event) => {
+    grow[1].style.transform = 'scale(1.5)';
+});
+grow[1].addEventListener('mouseout', (event) => {
+    grow[1].style.transform = 'scale(1)';
+});
+
+grow[2].addEventListener('mouseover', (event) => {
+    grow[2].style.transform = 'scale(1.5)';
+});
+grow[2].addEventListener('mouseout', (event) => {
+    grow[2].style.transform = 'scale(1)';
+});
+
+const image = document.querySelector('img');
+image.addEventListener('dblclick', (e) =>{
+    image.src = "img/bus-fire.jpg";
+});
+
